4876f055eb636bc6a82d8cdf7bf1ddc1266b1440
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-05 17:34:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,15 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 		monopoly = {
-		:railroads => {pieces: 4, rent_in_dollars: {a:1, b:2, c:3, d:4}}
+		:railroads => {
+			pieces: 4,
+			rent_in_dollars: {
+				names:1,
+				b:2,
+				c:3,
+				d:4
+			}
+			}
 	}
 end
 
