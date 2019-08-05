7106a37de6bc118b1bd1aa8569836bde0144be73
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-05 17:40:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,9 +26,9 @@ def monopoly_with_third_tier
 			:pieces => 4,
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
-				b:2,
-				c:3,
-				d:4
+				:two_pieces_owned => 50,
+				:three_pieces_owned => 75,
+				:four_pieces_owned => 100
 			},
 			:names => {
 				a:1,
